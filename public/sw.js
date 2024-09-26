cfee375647f397e59864235e8f9137836627ce16
Guillaume Pitot <guillaume@honeyinsurance.com>
2024-09-26 14:43:05 +1000

    logging

--- public/sw.js
+++ public/sw.js
@@ -2,6 +2,7 @@ console.log("hello world");
 
 // Listen for push messages and show a notification.
 self.addEventListener("push", (e) => {
+  console.log("NOTIFCATION RECEIVED", e);
   const data = e.data.json();
   // See https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
   self.registration.showNotification(data.title, data);
