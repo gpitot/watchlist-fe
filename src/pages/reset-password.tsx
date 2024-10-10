7eb285a41f045e9e81933e4649a2dcbe5182014e
Guillaume Pitot <guillaume@honeyinsurance.com>
2024-10-10 15:06:22 +1100

    dont redirect

--- src/pages/reset-password.tsx
+++ src/pages/reset-password.tsx
@@ -1,36 +1,10 @@
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { Session } from "@supabase/supabase-js";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { supabase } from "api/database";
-import { useNavigate } from "react-router-dom";
 
 export const ResetPasswordPage: React.FC = () => {
-  const [session, setSession] = useState<Session | null>(null);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-      if (session) {
-        navigate("/");
-      }
-    });
-
-    return () => subscription.unsubscribe();
-  }, []);
-
-  if (session) {
-    return null;
-  }
-
+  console.log("hostname ", window.location.hostname);
   return (
     <div className="p-4 ">
       <Auth.UpdatePassword
