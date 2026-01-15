eaafdd7000246358d9971a1f3c8784cfb0b4226f
Guillaume <guillaume.pitot@gmail.com>
2026-01-15 19:52:50 +1100

    Revert "try this"
    This reverts commit 447e528d6ef6289323014dfe8f08c25dbbe17498.

--- supabase/functions/add-movie-videos/index.ts
+++ supabase/functions/add-movie-videos/index.ts
@@ -2,7 +2,7 @@ import { corsHeaders } from "../_shared/cors.ts";
 import { Database } from "../_shared/database.types.ts";
 import { isMedium } from "../_shared/movie_service.ts";
 import { MovieAndShowService } from "../_shared/movie_service.ts";
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "supabase";
 
 import { z } from "zod";
 
--- supabase/functions/get-recently-available/index.ts
+++ supabase/functions/get-recently-available/index.ts
@@ -1,6 +1,6 @@
 import { corsHeaders } from "../_shared/cors.ts";
 import { Database } from "../_shared/database.types.ts";
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "supabase";
 import type { SendData } from "../_shared/email.ts";
 import { checkCronAuth } from "../_shared/cron-auth.ts";
 
