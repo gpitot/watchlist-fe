d74f571dad6702a9c1c8ee87ca28354028396d60
Guillaume <guillaume.pitot@gmail.com>
2026-01-17 14:31:00 +1100

    unused

--- src/components/formMutators.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-param-reassign */
-import { MutableState } from "final-form";
-
-export const clearFormErrors = <T>(_: object, form: MutableState<T>) => {
-  form.formState.submitError = undefined;
-  form.formState.errors = {};
-  form.formState.submitErrors = {};
-  form.formState.submitFailed = false;
-};
